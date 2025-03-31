d6f3ad53b56249b64b4fba026007854a3e6c15c3
sahiti <sahitivadlamani06@gmail.com>
2025-04-01 00:03:28 +0530

    added perks and ins

--- src/App.js
+++ src/App.js
@@ -6,8 +6,10 @@ import Navbar from './Components/Navbar';
 import Section1 from './Components/Section1';
 import Section2 from './Components/Section2';
 import Section3 from './Components/Section3';
+import Section4 from './Components/Section4';
 import Footer from './Components/Footer';
 import createAppTheme from './theme';
+
 
 function App() {
   // State for managing theme mode, defaulting to 'dark'
@@ -37,6 +39,7 @@ function App() {
           <Section1 />
           <Section2 />
           <Section3 />
+          <Section4 />
         </Box>
         <Footer />
       </Box>
---
+++ src/Components/Section2.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Section2 = () => {
+  return (
+    <div>Section1</div>
+  )
+}
+
+export default Section2
